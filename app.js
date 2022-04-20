5fd653e4705ae4ccb8b24d2170bf354ac472cdd1
MartinaGangemi <martinagangemi94@gmail.com>
2022-04-20 14:27:13 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      title: 'Hello Vue!',
+      img:"https://assets.reedpopcdn.com/news-persona-5-porting-altre-console-arrivo-atlus-rinnova-marchio-1641225335312.jpg/BROK/thumbnail/1600x800/format/jpg/quality/80/news-persona-5-porting-altre-console-arrivo-atlus-rinnova-marchio-1641225335312.jpg"
+    },
+    
+    
+  })
+
+
